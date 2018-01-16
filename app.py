465f38f2200ed7b284ef0145482ebfb0ee9684aa
Brock Smith <brocksmith225@gmail.com>
2018-01-16 19:17:56 +0000

    Added system to create account.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
-from flask import Flask, render_template
-import os
+from flask import Flask, render_template, request
+from flask.ext.login import LoginManager
+import os, uuid, psycopg2
 app = Flask(__name__, template_folder='pages')
+login_manager = LoginManager()
 
 @app.route("/")
 def opening():
@@ -10,5 +12,41 @@ def opening():
 @app.route("/ui")
 def ui():
     return render_template("ui.html")
+    
+@app.route("/create-account", methods=["POST"])
+def createAccount():
+    usr = request.form["username"]
+    pwd = request.form["password"]
+    email = request.form["email"]
+    
+    try:
+        conn = psycopg2.connect(dbname="unlock", user="ubuntu", password="Unl0ck", host="localhost")
+    except:
+        return render_template("unsuccessful-create-account.html")
+        
+    cur = conn.cursor()
+    
+    sql = "INSERT INTO users (usr, pwd, email, progress) values ('" + usr + "', '" + pwd + "', '" + email + "', 0);"
+    
+    try:
+        cur.execute(sql)
+    except:
+        return render_template("unsuccessful-create-account.html")
+    
+    conn.commit()
+    conn.close()
+    return render_template("login.html")
+    
+@app.route("/test")
+def test():
+    return render_template("unsuccessful-create-account.html")
+    
+class User:
+    is_authenticated = False
+    is_active = True
+    is_anonymous = True
+    u_id = uuid.uuid4()
+    def get_id():
+        return u_id
 
 app.run(host="0.0.0.0", port=8080, debug=True)
--- static/scripts/login.js
+++ static/scripts/login.js
@@ -23,6 +23,19 @@ function loginSubmit() {
     });
 }
 
-function login() {
-    
-}
+$("#create-account-form").submit(function() {
+    event.preventDefault();
+    usr = $("#create-account-form input[name='username']").val();
+    pwd = sha256($("#create-account-form input[name='password']").val());
+    confPwd = sha256($("#create-account-form input[name='confirm-password']").val());
+    email = $("#create-account-form input[name='email']").val();
+    if (pwd === confPwd) {
+        $("#create-account-submit input[name='username'").val(usr);
+        $("#create-account-submit input[name='email'").val(email);
+        $("#create-account-submit input[name='password'").val(pwd);
+        $("#create-account-submit").submit();
+    } else {
+        $("#create-account-form input[name='password']").val("");
+        $("#create-account-form input[name='confirm-password']").val("");
+    }
+});
