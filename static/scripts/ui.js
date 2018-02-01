4d0ba24c73c168827583c25d1225c10438b9555e
Brock Smith <brocksmith225@gmail.com>
2018-02-01 15:38:42 +0000

    Added loading symbol to flag check--this will be necessary for later as their should be some sort of feedback that the request was received while the site waits for an ajax response to determine whether or not the flag is correct.

--- static/scripts/ui.js
+++ static/scripts/ui.js
@@ -10,7 +10,7 @@ $(".ui-bottom").mouseover(function() {
 
 $(".ui-bottom").mouseout(function() {
     mouseOver = false;
-    if (!$(".flag-input").is(":focus") && !hintActive) {
+    if (!$(".flag-input").is(":focus") && $(".flag-check").html() == "CHECK" && !hintActive) {
         $(".ui-bottom").removeClass("active");
     }
 });
@@ -24,6 +24,17 @@ $(".flag-input").focusout(function() {
 $(".flag-input").focus(function() {
     $("#hint").hide();
     hintActive = false;
+});
+
+$(".flag-check").click(function() {
+    if ($(this).html() == "CHECK") {
+        $(this).html("<i class='fa fa-cog fa-spin'></i>");
+    } else {
+        $(this).html("CHECK");
+        if (!mouseOver) {
+            $(".ui-bottom").removeClass("active");
+        }
+    }
 });
 
 $("#hint-button").click(function() {
